5b0592b6e6f194217ddbe2575fc1008590fa6f64
Nao-as <alfo.gina9110@gmail.com>
2024-07-28 23:31:30 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Container, Title } from "@mantine/core";
+
+export default function Home() {
+	return (
+		<Container>
+			<Title>数独アプリ</Title>
+			<p className="text-lg mt-4">数独を解いてみよう！</p>
+		</Container>
+	);
+}
